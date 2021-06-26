383113b7c2f216d05a65ac8d4579c9053482cbc8
Akbarrul Mahrifat <akbarrulmahrifat@gmail.com>
2021-06-26 17:13:43 +0700

    update footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -8,10 +8,10 @@
 
 <!-- Main Footer -->
 <footer class="main-footer">
-	<strong>Copyright &copy; 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+	<strong>Copyright &copy; 2020-2021 <a href="<?=base_url()?>">Dewi Reformasi</a>.</strong>
 	All rights reserved.
 	<div class="float-right d-none d-sm-inline-block">
-		<b>Version</b> 3.1.0-pre
+		<b>Version</b> 1.0.0
 	</div>
 </footer>
 </div>
